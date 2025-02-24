bfcd2357caf7f11096815fb29bef12960edadfd8
Emilly Paiva <emillypaiva3260@gmail.com>
2025-02-23 21:13:20 -0300

    :tada: primeiro commit

---
+++ NumerosPrimos.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class NumerosPrimos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Quantos números deseja inserir? ");
+        int quantidade = scanner.nextInt();
+        
+        int[] numeros = new int[quantidade];
+
+      
+        System.out.println("Digite os números:");
+        for (int i = 0; i < quantidade; i++) {
+            numeros[i] = scanner.nextInt();
+        }
+
+        
+        System.out.println("Números primos na lista:");
+        for (int num : numeros) {
+            if (ehPrimo(num)) {
+                System.out.print(num + " ");
+            }
+        }
+
+        scanner.close();
+    }
+
+    
+    public static boolean ehPrimo(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
